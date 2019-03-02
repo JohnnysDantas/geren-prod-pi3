0cf71eaa31236d22ff62b14101ac571e28a48d2f
johnnys.dsantos <johnnys.dsantos@CAS555507W1012.senacsp.edu.br>
2019-03-01 21:38:54 -0300

    Inicial

---
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/GerenProd.java
@@ -0,0 +1,17 @@
+/*
+Autor:Johnnys Dantas
+*/
+
+package br.senac.tads.pi3.gerenprod;
+
+/**
+ *
+ * @author johnnys.dsantos
+ */
+public class GerenProd {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Teste Maven");
+    }
+}
